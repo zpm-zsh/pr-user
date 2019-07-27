5aef40e7e59350296dc9262ba61b6cd1dcfa04d0
Grigorii Horos <horosgrisa@gmail.com>
2019-07-28 02:03:49 +0300

    Remove return code

--- pr-user.plugin.zsh
+++ pr-user.plugin.zsh
@@ -1,53 +1,23 @@
 #!/usr/bin/env zsh
-PR_PROMPT_PREFIX=${PR_PROMPT_PREFIX:-' '}
-PR_PROMPT_SUFIX=${PR_PROMPT_SUFIX:-''}
+: ${PR_PROMPT_PREFIX:=' '}
+: ${PR_PROMPT_SUFIX:=''}
 
-
-_pr_user() {
-  
-  local RETVAL=$?
-  
-  local ISROOT=''
-  local RETSYMBOL=''
-  local USERSYMBOL=''
+if [[ $CLICOLOR != 0 ]]; then
   
   if [[ "$EUID" == 0 ]]; then
-    if [[ "$CLICOLOR" = 1 ]]; then
-      ISROOT="%{$c[red]${c_bold}${c_dim}%}root%{$c_reset%} "
-    else
-      ISROOT="root "
-    fi
+    USERSYMBOL="%{$c[red]$c_bold%}#%{$c_reset%}"
+  else
+    USERSYMBOL="%{$c[yellow]$c_bold%}$%{$c_reset%}"
   fi
   
-  if [[ $CLICOLOR = 1 ]]; then
-    
-    if [[ $RETVAL == 0 ]]; then
-      RETSYMBOL="%{$c[yellow]$c_bold%}"
-    else
-      RETSYMBOL="%{$c[red]$c_bold%}"
-    fi
-    
-  else
-    
-    if [[ "$RETVAL" == 0 ]]; then
-      RETSYMBOL='+'
-    else
-      RETSYMBOL='-'
-    fi
-    
-  fi
+else
   
   if [[ "$EUID" == 0 ]]; then
-    USERSYMBOL='#'
+    USERSYMBOL="#"
   else
-    USERSYMBOL='$'
+    USERSYMBOL="$"
   fi
   
-  if [[ $CLICOLOR = 1 ]]; then
-    USERSYMBOL+="%{$c_reset%}"
-  fi
-  
-  pr_user="$PR_PROMPT_PREFIX$ISROOT$RETSYMBOL$USERSYMBOL$PR_PROMPT_SUFIX"
-}
+fi
 
-precmd_functions+=(_pr_user)
+pr_user="${PR_PROMPT_PREFIX}${USERSYMBOL}${PR_PROMPT_SUFIX}"
