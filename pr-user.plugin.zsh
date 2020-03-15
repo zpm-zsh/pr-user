feede2922dd1e8e024c009530832cfa0a011ac96
Grigorii Horos <horosgrisa@gmail.com>
2020-03-15 17:50:13 +0200

    Optimize plugin

--- pr-user.plugin.zsh
+++ pr-user.plugin.zsh
@@ -1,18 +1,16 @@
 #!/usr/bin/env zsh
 
-typeset -g PR_PROMPT_PREFIX=${PR_PROMPT_PREFIX:-' '}
-typeset -g PR_PROMPT_SUFIX=${PR_PROMPT_SUFIX:-''}
+local PR_PROMPT_PREFIX=${PR_PROMPT_PREFIX:-' '}
+local PR_PROMPT_SUFIX=${PR_PROMPT_SUFIX:-''}
 
-if (( $+functions[zpm] )); then
-  zpm zpm-zsh/colors
-fi
+if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+  zpm zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
 
 typeset -g pr_user
 
-local USERSYMBOL="%{$c[red]$c_bold%}#%{$c_reset%}"
+pr_user="${PR_PROMPT_PREFIX}%{$c[red]$c_bold%}#%{$c_reset%}${PR_PROMPT_SUFIX}"
 
 if [[ "$EUID" != 0 ]]; then
-  USERSYMBOL="%{$c[yellow]$c_bold%}$%{$c_reset%}"
+  pr_user="${PR_PROMPT_PREFIX}%{$c[yellow]$c_bold%}$%{$c_reset%}${PR_PROMPT_SUFIX}"
 fi
-
-pr_user="${PR_PROMPT_PREFIX}${USERSYMBOL}${PR_PROMPT_SUFIX}"
