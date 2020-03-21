9626cdff76a87d81d38cad078eea92482e6d6049
Grigorii Horos <horosgrisa@gmail.com>
2020-03-21 13:29:18 +0200

    Optimize plugin

--- pr-user.plugin.zsh
+++ pr-user.plugin.zsh
@@ -3,14 +3,8 @@
 local PR_PROMPT_PREFIX=${PR_PROMPT_PREFIX:-' '}
 local PR_PROMPT_SUFIX=${PR_PROMPT_SUFIX:-''}
 
-if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-  zpm zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+typeset -g pr_user="${PR_PROMPT_PREFIX}%{$c[yellow]$c_bold%}$%{$c_reset%}${PR_PROMPT_SUFIX}"
 
-typeset -g pr_user
-
-pr_user="${PR_PROMPT_PREFIX}%{$c[red]$c_bold%}#%{$c_reset%}${PR_PROMPT_SUFIX}"
-
-if [[ "$EUID" != 0 ]]; then
-  pr_user="${PR_PROMPT_PREFIX}%{$c[yellow]$c_bold%}$%{$c_reset%}${PR_PROMPT_SUFIX}"
+if [[ "$EUID" == 0 ]]; then
+  pr_user="${PR_PROMPT_PREFIX}%{$c[red]$c_bold%}#%{$c_reset%}${PR_PROMPT_SUFIX}"
 fi
