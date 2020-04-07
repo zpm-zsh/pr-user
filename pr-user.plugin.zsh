f9f39436eb64f2422631ed5931a2bee7505c4016
Grigorii Horos <horosgrisa@gmail.com>
2020-04-07 03:08:15 +0300

    Optimize

--- pr-user.plugin.zsh
+++ pr-user.plugin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-local PR_PROMPT_PREFIX=${PR_PROMPT_PREFIX:-' '}
-local PR_PROMPT_SUFIX=${PR_PROMPT_SUFIX:-''}
+: ${PR_PROMPT_PREFIX:=' '}
+: ${PR_PROMPT_SUFIX:=''}
 
 typeset -g pr_user="${PR_PROMPT_PREFIX}%{$c[yellow]$c_bold%}$%{$c_reset%}${PR_PROMPT_SUFIX}"
 
