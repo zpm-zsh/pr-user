b8f8a4514c1f1389f756b16878652464f95dc1b2
Grigorii Horos <horosgrisa@gmail.com>
2023-06-13 14:20:19 +0000

    Change colors

--- pr-user.plugin.zsh
+++ pr-user.plugin.zsh
@@ -3,7 +3,7 @@
 : ${PR_PROMPT_PREFIX:=' '}
 : ${PR_PROMPT_SUFIX:=''}
 
-typeset -g pr_user="${PR_PROMPT_PREFIX}%{${c[yellow]}${c[bold]}%}$%{${c[reset]}%}${PR_PROMPT_SUFIX}"
+typeset -g pr_user="${PR_PROMPT_PREFIX}%{${c[orange]}${c[bold]}%}$%{${c[reset]}%}${PR_PROMPT_SUFIX}"
 
 if [[ "$EUID" == 0 ]]; then
   pr_user="${PR_PROMPT_PREFIX}%{${c[red]}${c[bold]}%}#%{${c[reset]}%}${PR_PROMPT_SUFIX}"
